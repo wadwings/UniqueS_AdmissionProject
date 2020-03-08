e9693a9278de33ac79723754dc721f52a298b1b1
wadwings <158802567@qq.com>
2020-03-08 15:42:39 +0800

    main finish

--- assignment/web前端/index.js
+++ assignment/web前端/index.js
@@ -1,13 +1,21 @@
 
 function change_width() {
-    winWidth = document.getElementsByClassName("h_background")[0].clientWidth;
-    var space=document.getElementsByClassName('space');
-    space[0].style.width = (winWidth - 260)/2 + "px";
-    space[0].style.height = 60 + "px";
-    space[1].style.width = (winWidth - 260)/2 + "px";
-    space[1].style.height = 60 + "px";
+    winWidth = document.getElementsByClassName("h_background")[0].clientWidth
+    var space=document.getElementsByClassName('space')
+    space[0].style.width = (winWidth - 260)/2 + "px"
+    space[0].style.height = 60 + "px"
+    space[1].style.width = (winWidth - 260)/2 + "px"
+    space[1].style.height = 60 + "px"
     var button=document.getElementById("button1");
     button.style.width = button.clientHeight*3.7 + "px"
+    document.getElementsByClassName("business")[0].style.width = (winWidth - 29.5*2) + 'px'
+    var footWidth = winWidth;
+    footer = document.getElementsByClassName('footer');
+    for (var i = 0; i < 3; i++)
+        footWidth -= footer[i].clientWidth
+    space = document.getElementsByClassName('space1')
+    space[1].style.width = footWidth/2 + "px"
+    space[0].style.width = footWidth/2 + "px"
 }
 function ready(){
     change_width();
